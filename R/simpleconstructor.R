8085bd4710266bfce2613baefd6a31be855c9eda
colin-fraser <colin.r.fraser@gmail.com>
2023-04-22 12:54:41 -0700

    remove simpleconstructor

--- R/simpleconstructor.R
+++ R/simpleconstructor.R
@@ -1,32 +1,3 @@
-simpleconstructor <- function(..., .prune = TRUE) {
-  argg <- c(as.list(environment()), list(...))
-  arg_names <- names(argg)
-  required_args <- names(Filter(\(x) x$required, argg))
-  enum_args <- names(Filter(\(x) !is.null(x$enum), argg))
-  f <- function(...) {
-    for (arg in required_args) {
-      if (is.null(env_get(nm = arg))) {
-        abort(paste0(arg, " is a required argument"))
-      }
-    }
-    for (arg in enum_args) {
-      env_poke(nm = arg, value = arg_match0(env_get(nm = arg), argg[[arg]][['enum']], arg_nm = arg))
-    }
-    out <- env_get_list(nms = arg_names)
-    if (.prune) {
-      out <- purrr::compact(out)
-    }
-    out
-  }
-  formals(f) <- map(argg, "enum")
-  f
-}
-
-#' @export
-property <- function(type = "string", enum = NULL, description = NULL, required = FALSE) {
-  structure(as.list(environment()), class = 'property')
-}
-
 #' Super Simple Constructor
 #'
 #' Generates a constructor function with the specified arguments, an optional custom class, and
@@ -52,7 +23,7 @@ property <- function(type = "string", enum = NULL, description = NULL, required 
 super_simple_constructor <- function(..., .class = c('supersimpleconstruct', 'list'), .prune = TRUE) {
   args <- function_args(...)
   f <- function(..., .prune = .prune) {
-    out <- env_get_list(nms = names(args))
+    out <- env_get_list(nms = names(args), default = stop("All arguments must be supplied"))
     if (.prune) {
       out <- purrr::compact(out)
     }
