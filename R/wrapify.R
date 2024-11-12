f24520242f60347bbce1a966e6cf417efcac4063
Colin Fraser <colinfraser@fb.com>
2024-11-12 12:38:38 -0800

    first rewrite commit

--- R/wrapify.R
+++ R/wrapify.R
@@ -3,55 +3,28 @@
 #' This function creates a wrapper object for an API with the given settings.
 #' The created wrapper can be used with the requestor function to make API calls.
 #'
-#' @param hostname The API hostname (e.g., "api.example.com")
-#' @param base_path The base path for the API (e.g., "/v1")
-#' @param auth_type The type of authentication to use (default: "none"). Accepts "none", "bearer", or "query".
-#' @param key_management The method for managing API keys (default: "none"). Accepts "none", "environment", or "ask".
-#' @param scheme The scheme to use for API requests (default: "https"). Accepts "http" or "https".
-#' @param user_agent The user agent string to use for API requests (default: "wrapify").
-#' @param default_content_type The default content type for API requests (default: "application/json").
+#' @param base_url The base URL
+#' @param default_headers The default headers for API requests
 #' @param default_query_args A named list of default query arguments to include in all API requests.
-#' @param env_var_name The name of the environment variable that stores the API key (required when key_management is set to "environment").
-#' @param credential_setter The name of the credential_setter function. This is used in the error message shown to the user when the credential can't be found.
+#' @param auth The authentication type
 #' @param ... Additional arguments passed to the wrapper object.
 #'
 #' @return A wrapper object with the provided settings.
 #' @export
 #' @import rlang
 #' @import httr2
-wrapper <- function(hostname, base_path, auth_type = "none",
-                    key_management = c('none', 'environment', 'ask'),
-                    scheme = "https", user_agent = "wrapify",
-                    default_content_type = "application/json",
-                    default_query_args = NULL,
-                    env_var_name = NULL,
-                    credential_setter = NULL,
-                    credential_type = c("json", "string"),
+wrapper <- function(base_url,
+                    default_headers = list(
+                      "content-type" = "application/json"
+                    ),
+                    default_query_args = list(),
+                    auth = auth_spec("none"),
                     ...) {
-  key_management <- match.arg(key_management)
-  credential_type <- match.arg(credential_type)
-  if (key_management == 'environment' && is.null(env_var_name)) {
-    abort("If key_management is set to \"environment\" then env_var_name must be supplied")
-  }
-
-  if (is.character(auth_type)) {
-    auth_type <- auth_type(auth_type)
-  }
-
   list(
-    url = list(
-      hostname = hostname,
-      path = base_path,
-      scheme = scheme
-    ),
-    user_agent = user_agent,
-    default_content_type = default_content_type,
-    auth_type = auth_type,
-    key_management = key_management,
+    base_url = base_url,
+    default_headers = default_headers,
     default_query_args = default_query_args,
-    env_var_name = env_var_name,
-    credential_setter = credential_setter,
-    credential_type = credential_type,
+    auth = auth,
     ...
   )
 }
@@ -84,6 +57,7 @@ wrapper <- function(hostname, base_path, auth_type = "none",
 #'
 #' @return A custom requestor function for making API calls with the specified settings. The returned function will have the specified resource_args, query_args, and other arguments.
 #' @export
+#' @import rlang
 requestor <- function(wrapper,
                       resource,
                       resource_args = NULL,
@@ -93,26 +67,25 @@ requestor <- function(wrapper,
                       body_args = NULL,
                       body_constants = NULL,
                       method = "get",
-                      default_action = c("perform", "dryrun"),
                       content_type = NULL,
-                      body_type = 'json',
+                      body_type = "json",
                       additional_request_args = NULL,
-                      decode_if_success_default_value = TRUE,
-                      post_process_default_value = TRUE,
-                      post_processor = NULL) {
-  default_action <- match.arg(default_action)
-  content_type <- content_type %||% default_content_type(wrapper)
-  args <- c(resource_args,
-            query_args,
-            wrapper$default_query_args,
-            body_args)
-  f <- function(..., credentials = default_credentials(wrapper),
-                action = default_action,
-                decode_if_success = decode_if_success_default_value,
-                post_process = post_process_default_value) {
-    if (is.null(post_processor)) {
-      post_process <- FALSE
+                      perform_by_default = TRUE,
+                      extract_body_by_default = TRUE,
+                      extractor = "infer") {
+  args <- c(
+    resource_args,
+    query_args,
+    wrapper$default_query_args,
+    body_args
+  )
+  f <- function(..., .credentials = get_credentials_from_wrapper(wrapper), .perform = perform_by_default, .extract = extract_body_by_default,
+                .extractor = extractor) {
+    for (arg in fn_fmls_syms()) {
+      # TODO: get required argument checking to work
+      check_required(arg)
     }
+
     if (length(query_args) > 0 || length(wrapper$default_query_args) > 0) {
       query_args <- rlang::env_get_list(nms = c(names(query_args), names(wrapper$default_query_args)))
     }
@@ -124,140 +97,113 @@ requestor <- function(wrapper,
     }
     query_payload <- c(query_args, query_constants)
     resource_arg_values <- c(resource_args, resource_constants)
-    out <- request_from_wrapper(wrapper) |>
+
+    out <- request_from_wrapper(wrapper, .credentials) |>
       req_method(method) |>
       req_url_path_append(glue_url(resource, resource_arg_values)) |>
-      req_url_query(!!!query_payload) |>
-      authorize(wrapper, credentials)
+      req_url_query(!!!query_payload)
 
     if (length(body_args) > 0) {
       out <- out |>
         req_body_json(purrr::compact(body_args))
     }
 
-    if (action == "perform") {
-      out <- req_perform(out)
+    if (!.perform) {
+      return (out)
+    }
 
-      if (decode_if_success && !resp_is_error(out)) {
-        out <- decoder(content_type)(out)
-      }
+    out <- req_perform(out)
 
-      if (post_process && !is.null(post_processor)) {
-        out <- post_processor(out)
-      }
-    } else if (action == "dryrun") {
-      out <- req_dry_run(out)
+    if (.extract) {
+      extractor <- get_extractor(.extractor)
+      out <- extractor(out)
     }
 
     out
   }
+
   f_args <- c(args, formals(f))
-  if (is.null(post_processor)) {
-    f_args$post_process <- NULL
-  }
   formals(f) <- f_args
   f
 }
 
-wrapper_auth_type <- function(wrapper) wrapper$auth_type$type
-
-#' @export
-ask_for_credentials <- function(wrapper) {
-  if (wrapper_auth_type(wrapper) == 'query') {
-    set_names(lapply(wrapper$auth_type$param_names, \(x) getPass::getPass(x)),
-              wrapper$auth_type$param_names)
-  } else if (wrapper_auth_type(wrapper) == 'bearer') {
-    getPass::getPass("API Key")
+get_extractor <- function(x) {
+  if (is.function(x)) {
+    return(x)
   }
-}
-
-default_content_type <- function(wrapper) wrapper$default_content_type
-
-default_credentials <- function(wrapper) {
-  switch(wrapper$key_management,
-         "none" = NULL,
-         "environment" = get_credential_from_environment(wrapper),
-         "ask" = ask_for_credentials(wrapper))
-}
-
-#' Create a credential manager function for an API wrapper
-#'
-#' This function generates a custom credential manager function for the given
-#' API wrapper. The credential manager function is responsible for storing the
-#' API credentials in the environment variable specified in the wrapper.
-#' This function only works when the `key_management` option of the wrapper
-#' is set to "environment".
-#'
-#' @param wrapper The API wrapper object initialized using the `wrapper` function.
-#' @importFrom jsonlite toJSON
-#'
-#' @return A custom credential manager function that stores the API credentials
-#'         in the environment variable specified in the wrapper. When called,
-#'         the returned function collects the API credentials from the user
-#'         using `ask_for_credentials` and stores them as a JSON string
-#'         in the environment variable.
-#' @export
-credential_setter <- function(wrapper) {
-  key_management <- wrapper$key_management
-
-  stopifnot("`key_management` must be 'environment'" = key_management == "environment",
-            "`env_var_name` must be supplied" = !is.null(wrapper$env_var_name))
-
-  env_var_name <- wrapper$env_var_name
-
-  function(credentials = ask_for_credentials(wrapper)) {
-    json_credentials <- toJSON(credentials, auto_unbox = TRUE)
-    do.call(Sys.setenv, list2("{env_var_name}" := json_credentials))
+  if (x == "infer") {
+    return(infer_extractor)
   }
+  getFunction(x)
 }
 
-#' @importFrom jsonlite fromJSON
-get_credential_from_environment <- function(wrapper) {
-  credential <- Sys.getenv(wrapper$env_var_name)
-  if (is.null(credential) || credential == "") {
-    msg <- paste("Credentials not found in environment variable:", wrapper$env_var_name)
-    if (!is.null(wrapper$credential_setter)) {
-      msg <- paste(msg, "\nTry running", paste0(wrapper$credential_setter, "()"))
-    }
-    abort(msg)
-  }
-  if (wrapper$credential_type == "json") {
-    credential <- fromJSON(credential, simplifyVector = FALSE)
-  }
-  credential
-}
-
-base_url <- function(wrapper) {
-  httr2::url_build(wrapper$url)
-}
-
-request_from_wrapper <- function(wrapper) {
-  req <- request(base_url(wrapper)) |>
-    req_user_agent(wrapper$user_agent)
-
-  req
-}
-
-glue_url <- function(url, values) {
-  for (nm in names(values)) {
-    url <- gsub(paste0("\\{", nm, "\\}"), urltools::url_encode(values[[nm]]), url)
-  }
-  url
-}
-
-authorize <- function(req, wrapper, credentials) {
-  switch(wrapper_auth_type(wrapper),
-         "none" = req,
-         "bearer" = req_auth_bearer_token(req, credentials),
-         "query" = req_url_query(req, !!!credentials)
-  )
-}
-
-decoder <- function(content_type) {
-  switch(content_type,
+infer_extractor <- function(resp) {
+  f <- switch(resp_content_type(resp),
     "application/json" = resp_body_json
   )
+  f(resp)
 }
+
+request_from_wrapper <- function(wrapper, credentials) {
+  r <- request(wrapper$base_url) |>
+    req_headers(!!!wrapper$default_headers) |>
+    req_url_query(!!!wrapper$default_query_args)
+
+  authorize(r, wrapper, credentials)
+}
+
+
+glue_url <- function(template, params) {
+  glue::glue_data(params, template)
+}
+
+authorize <- function(req, wrapper, credential) {
+  if (wrapper$auth$type == "none") {
+    return(req)
+  }
+
+  if (wrapper$auth$type == "bearer") {
+    return(req_auth_bearer_token(req, credential))
+  }
+
+  if (wrapper$auth$type == "header") {
+    h <- list2(`wrapper$auth$header` := credential)
+    return(req_headers(req, !!! h))
+  }
+}
+
+
+
+get_credentials_from_wrapper <- function(wrapper) {
+  switch(wrapper$auth$type,
+    "none" = NULL,
+    "bearer" = get_env_var_from_wrapper(wrapper),
+    "header" = get_env_var_from_wrapper(wrapper)
+  )
+}
+
+get_env_var_from_wrapper <- function(wrapper) {
+  if (is.null(wrapper$env_var_name)) {
+    stop("Wrapper has no default environment variable name")
+  }
+
+  out <- Sys.getenv(wrapper$env_var_name)
+
+  if (out == "") {
+    abort(glue::glue("No API key found for environment variable {wrapper$env_var_name}"))
+  }
+
+  out
+}
+
+auth_spec <- function(type, header = NULL) {
+  switch(type,
+    "none" = list(type = "none"),
+    "bearer" = list(type = "bearer"),
+    "header" = list(type = "header", header = header)
+  )
+}
+
 
 #' @export
 function_args <- function(...) {
--- tests/testthat/test-wrapify.R
+++ tests/testthat/test-wrapify.R
@@ -1,151 +1,70 @@
-test_wrapper <- wrapper(
-  hostname = "api.example.com",
-  base_path = "/v1",
-  auth_type = "none",
-  key_management = "none",
-  scheme = "https",
-  user_agent = "test_agent",
-  default_content_type = "application/json"
+test_that("Wrapper construction", {
+  example <- wrapper(example_url())
+  expect_equal(example$auth$type, "none")
+  expect_equal(example$default_headers$`content-type`, "application/json")
+})
+
+test_that("Example iris requestor", {
+  example <- wrapper(example_url())
+
+  iris <- requestor(
+    example,
+    "iris",
+    query_args = function_args(
+      page = ,
+      limit = 1
+    )
+  )
+
+  expect_equal(length(iris(1)$data), 1)
+  expect_equal(length(iris(1, 2)$data), 2)
+
+  custom_extractor <- function(resp) {
+    l <- resp |>
+      httr2::resp_body_json()
+    l$count
+  }
+
+  expect_equal(iris(1, 2, .extractor = custom_extractor), 150)
+
+})
+
+test_that("Example openai wrapper", {
+  openai <- wrapper(
+    "https://api.openai.com/v1",
+    auth = auth_spec("bearer")
+  )
+
+  list_models <- requestor(
+    openai,
+    "models"
+  )
+
+  expect_error(list_models(.perform = FALSE), "Wrapper has no default environment variable name")
+  expect_equal(resp_status(list_models(.credentials = Sys.getenv("OPENAI_KEY"), .extract = FALSE)),
+               200)
+})
+
+test_that("Example Anthropic wrapper", {
+  anth <- wrapper(
+    "https://api.anthropic.com/v1",
+    auth = auth_type("header", header = "x-api-key"),
+    default_headers = list(
+      "anthropic-version" = "2023-06-01"
+    ),
+    env_var_name = "ANTHROPIC_API_KEY"
+  )
+
+  message <- requestor(
+    anth,
+    "messages",
+    body_args = function_args(
+      messages =,
+      max_tokens = 1024,
+      model = "claude-3-5-sonnet-20241022"
+    )
+  )
+
+  message(list(role = "user", content = "Hello there"), .perform = FALSE)
+}
 )
-
-test_that("wrapper function creates a valid wrapper object", {
-  expect_named(test_wrapper, c("url", "user_agent", "default_content_type", "auth_type", "key_management", "default_query_args", "env_var_name", "credential_setter", "credential_type"))
-  expect_equal(test_wrapper$url$hostname, "api.example.com")
-  expect_equal(test_wrapper$url$path, "/v1")
-  expect_equal(test_wrapper$url$scheme, "https")
-  expect_equal(test_wrapper$user_agent, "test_agent")
-  expect_equal(test_wrapper$default_content_type, "application/json")
-  expect_equal(test_wrapper$auth_type$type, "none")
-  expect_equal(test_wrapper$key_management, "none")
-  expect_null(test_wrapper$default_query_args)
-  expect_null(test_wrapper$env_var_name)
-})
-
-# Create a test requestor function for the "/users" endpoint
-test_requestor <- requestor(
-  test_wrapper,
-  "users",
-  query_args = function_args(userid = , fields = "all")
-)
-
-test_that("requestor function creates a valid requestor function", {
-  expect_type(test_requestor, "closure")
-  expect_named(formals(test_requestor), c("userid", "fields", "...", "credentials", "action", "decode_if_success"))
-  expect_equal(formals(test_requestor)$fields, "all")
-})
-
-test_that("requestor function with post_processor", {
-  test_requestor <- requestor(test_wrapper, "users",
-                              query_args = function_args(userid = ),
-                              post_processor = \(x) 1)
-  expect_named(formals(test_requestor), c("userid", "...", "credentials", "action",
-                                          "decode_if_success", "post_process"))
-})
-
-# Test the requestor function with a dry run
-test_that("requestor function performs a dry run correctly", {
-  response <- test_requestor(userid = 1, action = "dryrun")
-  expect_type(response, "list")
-  expect_equal(response$method, "GET")
-  expect_equal(response$headers$`user-agent`, "test_agent")
-})
-
-
-
-
-
-test_that("requestor function retrieves a user from the JSONPlaceholder API", {
-  jsonplaceholder_wrapper <- wrapper(
-    hostname = "jsonplaceholder.typicode.com",
-    base_path = "",
-    auth_type = "none",
-    key_management = "none",
-    scheme = "https",
-    user_agent = "test_agent",
-    default_content_type = "application/json"
-  )
-
-  users_requestor <- requestor(
-    jsonplaceholder_wrapper,
-    "users",
-    query_args = function_args(id = )
-  )
-
-  # Make a request to the "/users" endpoint with a specific user ID
-  response <- users_requestor(id = 1, action = "perform")
-
-  # Check if the user object has the expected properties
-  expect_named(response[[1]], c("id", "name", "username", "email", "address", "phone", "website", "company"))
-
-  # Check if some properties have the expected values
-  expect_equal(response[[1]]$id, 1)
-
-  users_requestor_pp <- requestor(
-    jsonplaceholder_wrapper,
-    "users",
-    query_args = function_args(id = ),
-    post_processor = \(x) x[[1]]$company$name
-  )
-
-  response_2 <- users_requestor_pp(id = 1, post_process = FALSE)
-  expect_equal(response, response_2)
-  response_3 <- users_requestor_pp(id = 1, post_process = TRUE)
-  expect_equal(response_3, "Romaguera-Crona")
-})
-
-test_that("credential_setter stores and retrieves API credentials correctly", {
-  # Create a test wrapper object
-  test_wrapper <- wrapper(
-    "test.com",
-    "api",
-    key_management = "environment",
-    env_var_name = "TEST_API_CREDENTIALS"
-  )
-
-  # Test single-value credentials
-  test_credential_setter <- credential_setter(test_wrapper)
-  test_credential_setter(credentials = "test_api_key")
-
-  retrieved_credentials <- get_credential_from_environment(test_wrapper)
-  expect_equal(retrieved_credentials, "test_api_key")
-
-  # Test named list credentials (query auth type)
-  test_query_credentials <- list(api_key = "test_key", api_secret = "test_secret")
-  test_credential_setter(credentials = test_query_credentials)
-
-  retrieved_credentials <- get_credential_from_environment(test_wrapper)
-  expect_equal(retrieved_credentials, test_query_credentials)
-})
-
-test_that("get_credential_from_environment includes credential_setter message if present", {
-  Sys.unsetenv("MY_CREDENTIALS")
-
-  mock_wrapper <- list(
-    env_var_name = "MY_CREDENTIALS",
-    credential_setter = "my_credential_setter"
-  )
-
-  expected_error_msg <- "Credentials not found in environment variable: MY_CREDENTIALS \nTry running my_credential_setter()"
-  expect_error(get_credential_from_environment(mock_wrapper), expected_error_msg)
-
-  mock_wrapper <- list(
-    env_var_name = "MY_CREDENTIALS",
-    credential_setter = NULL
-  )
-
-  expect_error(get_credential_from_environment(mock_wrapper),
-               "Credentials not found in environment variable: MY_CREDENTIALS")
-})
-
-test_that("get_credential_from_environment returns the correct credentials", {
-  Sys.setenv("MY_CREDENTIALS" = '{"username": "my_username", "password": "my_password"}')
-  mock_wrapper <- wrapper(
-    "test.com",
-    "api",
-    env_var_name = "MY_CREDENTIALS",
-    credential_setter = NULL
-  )
-  expected_result <- list(username = "my_username", password = "my_password")
-  actual_result <- get_credential_from_environment(mock_wrapper)
-  expect_equal(actual_result, expected_result)
-})
